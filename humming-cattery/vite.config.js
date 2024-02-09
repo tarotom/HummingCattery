9c3102436798b9614a284a6ac35d23341dc7dd35
Tomi <ot_tomi@hotmail.com>
2024-02-09 21:59:10 +0200

    Update base address in vite.config.js

--- humming-cattery/vite.config.js
+++ humming-cattery/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/humming-cattery/",
+  base: "/HummingCattery/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
