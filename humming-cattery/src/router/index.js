59b13bfc262438600a667eac781500c0712cf5b4
Tomi <ot_tomi@hotmail.com>
2024-02-09 22:25:20 +0200

    Update router index.js

--- humming-cattery/src/router/index.js
+++ humming-cattery/src/router/index.js
@@ -1,23 +1,56 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/Mollies",
+    name: "Mollies",
+    component: Mollies,
+  },
+  {
+    path: "/Tomcats",
+    name: "Tomcats",
+    component: Tomcats,
+  },
+  {
+    path: "/Castrates",
+    name: "Castrates",
+    component: Castrates,
+  },
+  {
+    path: "/InMemoriam",
+    name: "InMemoriam",
+    component: InMemoriam,
+  },
+  {
+    path: "/News",
+    name: "News",
+    component: News,
+  },
+  {
+    path: "/KittenBasket",
+    name: "KittenBasket",
+    component: KittenBasket,
+  },
+  {
+    path: "/KittenInfo",
+    name: "KittenInfo",
+    component: KittenInfo,
+  },
+  {
+    path: "/Breeder",
+    name: "Breeder",
+    component: Breeder,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
